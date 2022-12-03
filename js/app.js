3685bc7c80dd7d74de9c3ffe738bc6d4e99672b2
samuelcleo <cleophassamuel@gmail.com>
2022-12-03 13:11:35 +1100

    Created a Search Function
    Implemented a search function using keyup event listener => storing target value in lowercase.
    Then querying for all 'a' selectors to check for each datacaption includes stored value.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 baguetteBox.run('.gallery', {
     captions: true
 });
+
+const searchInput = document.getElementById('search'); 
+searchInput.addEventListener('keyup', e => {
+    let currentValue = e.target.value.toLowerCase();
+    let links = document.querySelectorAll('a');
+    
+    links.forEach(link => {
+        if (link.getAttribute('data-caption').toLowerCase().includes(currentValue)) {
+            link.style.display = 'block';
+        } else {
+            link.style.display = 'none';
+        }
+    })
+});
